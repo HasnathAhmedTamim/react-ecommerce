30ceafb9e9425149c8b5f132ce9fd199a48d2ae5
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-02-08 15:39:06 +0600

    cart and checkout design

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignUpPage from "./pages/SignUpPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./features/counter copy/Cart";
 import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage></CartPage>,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout></Checkout>,
   },
 ]);
 
